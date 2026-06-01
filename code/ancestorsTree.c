5cc8e5ecf068cb5ae101ef7f208c4674bc93cc8c
PRossell <rossellm.pau@gmail.com>
2026-06-02 01:47:59 +0200

    darreres modificacions

---
+++ code/ancestorsTree.c
@@ -0,0 +1,84 @@
+#include "./small.h"
+#include <stdio.h>
+#include <string.h>
+
+struct FamilyTreeNode * familyTreeDFS = NULL;   // Family Tree que plenarem amb DFS
+struct FamilyTreeNode * familyTreeBFS = NULL;   // Family Tree que plenarem amb BFS
+
+struct parents {    // Aquesta estructura em feia falta per retornar una parella de integers en una funcio
+    int mParentsID;
+    int fParentsID;
+};
+
+
+struct parents fillNode(int citiesInfoIDX, struct FamilyTreeNode * node){    
+    // Aquesta funcio senzillament emplena un node amb els valors corresponents, perque fa pal fer-ho cada cop :)
+    // RETORNA L'INDEX DE MOTHER'S PARENTS I FATHER'S PARENTS
+
+    strcpy(node->motherName, citiesInfo[citiesInfoIDX].mother_name);
+    strcpy(node->fatherName, citiesInfo[citiesInfoIDX].father_name);
+    node->city_id = citiesInfoIDX;
+    node->mother_parents = NULL;
+    node->father_parents = NULL;
+    int mothers_parents_id = citiesInfo[citiesInfoIDX].mother_parents_city_id;
+    int fathers_parents_id = citiesInfo[citiesInfoIDX].father_parents_city_id;
+    
+    struct parents p;
+    p.mParentsID = mothers_parents_id;
+    p.fParentsID = fathers_parents_id;
+    return p;
+}
+
+void dfsStep(struct FamilyTreeNode * current, int nextIdx){
+    // Aquesta funcio senzillamen es el pas de dfs que es truca de forma recursiva
+    struct parents p = fillNode(nextIdx, current);
+
+    if (!(p.mParentsID == -1 && p.fParentsID == -1)){
+        dfsStep(current->mother_parents, p.mParentsID);
+        dfsStep(current->father_parents, p.fParentsID);
+    }
+}
+
+
+void dfsSearch(struct FamilyTreeNode * head){
+    // I aquesta es la ultima de dfs que senzillament inicialitza el familytree si no esta inicialitzat, i truca dfsStep
+    
+    if (head == NULL){
+    }
+
+    struct parents p = fillNode(0, head);
+
+
+    dfsStep(head->mother_parents, p.mParentsID);
+    dfsStep(head->father_parents, p.fParentsID);
+}
+
+
+
+
+void printTree(struct FamilyTreeNode *node)
+{
+    if (node == NULL) {
+        return;
+    }
+
+    printf("%s / %s - %s\n",
+           node->motherName,
+           node->fatherName,
+           citiesInfo[node->city_id].city_name);
+
+    printTree(node->mother_parents);
+    printTree(node->father_parents);
+}
+
+int main()
+{
+    dfsSearch(familyTreeDFS);
+    printf("DFS family tree:\n");
+    printTree(familyTreeDFS);
+
+    printf("\nBFS family tree:\n");
+    printTree(familyTreeBFS);
+
+    return 0;
+}
---
+++ code/worldJourney.c
@@ -0,0 +1,177 @@
+#include "small.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct RoadMap *head = NULL;
+
+struct RoadMap *totalRoadMap = NULL;
+
+struct RoadMap *getLastElement(struct RoadMap *head)
+{
+    struct RoadMap *tmp = head;
+
+    if (head == NULL) {
+        return NULL;
+    }
+
+    while (tmp->next != NULL) {
+        tmp = tmp->next;
+    }
+
+    return tmp;
+}
+
+void addToRoadMap(int city_id, int hop_cost)
+{
+    struct RoadMap *newNode = malloc(sizeof(struct RoadMap));
+    newNode->city_id = city_id;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        newNode->total_cost = 0;
+        head = newNode;
+    } else {
+        struct RoadMap *lastElement = getLastElement(head);
+        newNode->total_cost = lastElement->total_cost + hop_cost;
+        lastElement->next = newNode;
+    }
+
+    //Afegeixo lo mateix x el total roadmap
+    struct RoadMap *newTotalNode = malloc(sizeof(struct RoadMap));
+    newTotalNode->city_id = city_id;
+    newTotalNode->total_cost = newNode->total_cost;
+    newTotalNode->next = NULL;
+
+    if (totalRoadMap == NULL) {
+        totalRoadMap = newTotalNode;
+    } else {
+        struct RoadMap *lastTotalElement = getLastElement(totalRoadMap);
+        lastTotalElement->next = newTotalNode;
+    }
+}
+
+void printRoadMap()
+{
+    struct RoadMap *current = head;
+
+    while (current != NULL) {
+        if (current->next == NULL) {
+            printf("%s", citiesInfo[current->city_id].city_name);
+            printf("  %d", current->total_cost);
+        } else {
+            printf("%s - ", citiesInfo[current->city_id].city_name);
+        }
+        current = current->next;
+    }
+
+    printf("\n");
+}
+
+void printTotalRoadMap(int totalCost)
+{
+    printf("\n\nTotal Road Map: \n");
+
+    struct RoadMap *tmp = totalRoadMap;
+    int currentCityID = -1;
+    while (tmp != NULL) {
+        if (tmp->next == NULL) {   // Aqui he afegit aquesta variable currentCityID que serveix perque no s'ens imprimeixi la mateixa ciutat dos cops seguits :)
+            printf("%s", citiesInfo[tmp->city_id].city_name);
+        } else if( tmp->city_id != currentCityID){
+            printf("%s - ", citiesInfo[tmp->city_id].city_name);
+            currentCityID = tmp->city_id;
+        }
+        tmp = tmp->next;
+    }
+
+    printf("\n\nTotal Cost: %d\n", totalCost);
+}
+
+void deleteAllRoadMap()
+{
+    struct RoadMap *current = head;
+
+    while (current != NULL) {
+        struct RoadMap *tmp = current->next;
+        free(current);
+        current = tmp;
+    }
+
+    head = NULL;
+}
+
+int RouteSearch(int source, int destination, struct RoadMap *RoadMap)
+{
+    int last_visited = NUMBER_CITIES; //Mirem d'on venim per no entrar en bucle
+
+    if (RoadMap == NULL) { // Aixo nomes quan no hi ha res a RoadMap (l'inicialitzem)
+        addToRoadMap(source, 0);
+        last_visited = -1;
+    };
+
+    int cost = adjacency_matrix[source][destination];
+
+    if (cost != 0) { // Aixo es quan hi ha ruta directa de source a destination
+        addToRoadMap(destination, cost);
+        return cost;
+    }
+
+    // A partir d'aqui ens trobem en el punt en que hem de fer escala (a la ciutat mes barata)
+    if ((last_visited != -1) && (head != NULL) && (head->next != NULL)) {
+        struct RoadMap *tmp = head;
+
+        while (tmp->next->next != NULL) { // AIXO NO FUNCIONA QUAN NOMES HI HA UN ITEM A ROADMAP
+            tmp = tmp->next;
+        }
+
+        last_visited = tmp->city_id;
+    }
+
+    int min = 0; //
+    int idx = 0;
+
+    while ((idx < NUMBER_CITIES) && (!min)) {
+        if (idx != last_visited) {
+            min = adjacency_matrix[source][idx];
+        }
+        idx++;
+    }
+
+    int idxNextSource = idx - 1;
+
+    for (; idx < NUMBER_CITIES; idx++) {
+        if ((adjacency_matrix[source][idx] < min) && (adjacency_matrix[source][idx] != 0) && (idx != last_visited)) {
+            min = adjacency_matrix[source][idx];
+            idxNextSource = idx;
+        }
+    }
+
+    cost = min;
+    addToRoadMap(idxNextSource, cost);
+
+    return RouteSearch(idxNextSource, destination, head) + cost;
+};
+
+//dfsSearch
+
+
+int main()
+{
+    int cost = 0;
+
+    cost = cost + RouteSearch(0, 5, head);
+    printf("\nPartial Road Map: \n");
+    printRoadMap();
+    deleteAllRoadMap();
+
+    cost = cost + RouteSearch(5, 6, head);
+    printRoadMap();
+    deleteAllRoadMap();
+
+    cost = cost + RouteSearch(6, 1, head);
+    printRoadMap();
+    deleteAllRoadMap();
+
+    printTotalRoadMap(cost);
+
+    return 0;
+}
